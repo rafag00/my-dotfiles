55b1e101cc5ed35fe507d94a68ccdf0b457e97d2
Rafael Gonçalves <57418462+rafag00@users.noreply.github.com>
2025-10-16 23:44:56 +0100

    Niri updates

--- nix/home-manager/modules/niri.nix
+++ nix/home-manager/modules/niri.nix
@@ -125,9 +125,71 @@ in
         };
         
         layout = {
+
           gaps = 8;
-          focus-ring.enable = true;
+
+          center-focused-column = "on-overflow";
+
+          always-center-single-column = true;
+
+          background-color = "transparent";
+
+          preset-column-widths = [
+            { proportion = 1. / 3.; }
+            { proportion = 1. / 2.; }
+            { proportion = 2. / 3.; }
+          ];
+
+          # To decide if the border is realy necessary as focus-ring is the normal behaviour
+          focus-ring = {
+            enable = true;
+            width = 4;
+            active.color = "#7fc8ff";
+            inactive.color = "#505050";
+            urgent.color = "#9b0000";
+          };
+
+          #The border is a decoration drawn inside every window in the layout. It will take space away from windows. That is, if you have a border of 8px, then each window will be 8px smaller on each edge than if you had no border.
+          #The currently focused window, i.e. the window that can receive keyboard input, will be drawn according to layout.border.active, and all other windows will be drawn according to layout.border.inactive.
+          border = {
+            enable = true;
+            width = 4;
+            active.color = "#7fc8ff";
+            inactive.color = "#505050";
+            urgent.color = "#9b0000";
+          };
+
+          shadow = {
+            enable = true;
+            color = "#00000070";
+            draw-behind-window = false; #Default don't know if needed
+            inactive-color = "#00000070"; # Can be removed
+            offset.x = 0.000000; # Default
+            offset.y = 5.000000; # Default
+            softness = 30.000000; # Default
+            spread = 5.000000; # Default
+
+          };
+
+          # Don't know what this does
+          tab-indicator = {
+            enable = true;
+            corner-radius = 0.000000; # Default
+            gap = 5.000000; # Default
+            gaps-between-tabs = 0.000000; # Default
+            hide-when-single-tab = false; # Default
+            length.total-proportion = 0.500000; # Default
+            place-within-column = false; # Default
+            position = "top";
+            width = 4.000000; # Default
+            # active, inactive and urgent colors
+          };
+
         };
+
+        prefer-no-csd = true;
+
+        screenshot-path = "~/Pictures/Screenshots/Screenshot from %Y-%m-%d %H-%M-%S.png";
         
         input = { 
           keyboard.xkb.layout = "eu, pt";
@@ -151,14 +213,52 @@ in
           workspace-auto-back-and-forth = true;
         };
 
+        cursor = {
+          theme = "breeze"; # Maybe change maybe not
+          size = 24;
+          hide-after-inactive-ms = 20000;
+          hide-when-typing = false;
+
+        };
+
         environment = {
           DISPLAY = ":0";
           ELECTRON_OZONE_PLATFORM_HINT = "auto";
+          NIXOS_OZONE_WL = "1";
+          CLUTTER_BACKEND = "wayland";
+          GDK_BACKEND = "wayland";
+          QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+          QT_QPA_PLATFORM = "wayland";
+          QT_QPA_PLATFORMTHEME = "qtct";
+          #QT_STYLE_OVERRIDE = "kvantum";
+          QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
         };
+
+        workspaces."1" = {
+          name = "1";
+          # open-on-output is an option but needs multi-screen testing
+          };
+        workspaces."2" = { name = "2"; };
+        workspaces."3" = { name = "3"; };
+        workspaces."Chats" = { name = "Chats"; };
+        workspaces."Misc" = { name = "Misc"; };
+
+        window-rules = [
+          {
+            matches = [
+              {
+                app-id = "^KeePassXC$";
+              }
+            ];
+            open-on-workspace = "Misc";  
+            
+          }
+        ];
 
         spawn-at-startup = [
           { command = [ "xwayland-satellite" ]; }
           { command = [ "noctalia-shell" ]; }
+          
           # {
           # command = [
           #   "sh"
--- nix/nixos/modules/niri.nix
+++ nix/nixos/modules/niri.nix
@@ -1,4 +1,10 @@
 {pkgs, lib, ... }: {
   programs.niri.enable = true; 
   programs.niri.package = pkgs.niri-stable;
+
+
+  environment.systemPackages = with pkgs; [
+    #TO-Do
+    xwayland-satellite
+  ];
 }
